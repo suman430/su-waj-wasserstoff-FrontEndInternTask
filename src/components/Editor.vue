1488517e629e858c580d8211396e47535b634828
suman430 <sumanbhardwaj382@gmail.com>
2025-05-11 14:35:29 +0530

    texteditor

---
+++ src/components/Editor.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import {useEditor, EditorContent} from "@tiptap/vue-3";
+import * as Y from "yjs"
+import Collaboration from "@tiptap/extension-collaboration"
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+
+import { WebrtcProvider } from "y-webrtc";
+import StarterKit from "@tiptap/starter-kit";
+const doc = new Y.Doc();
+const provider = new WebrtcProvider("text-editor",doc);
+const editor= useEditor({
+  content:"<p>Hello world!</p>",
+  extensions: [StarterKit, 
+  Collaboration.configure({
+document: doc,
+  }),
+  CollaborationCursor.configure({
+    provider: provider,
+    user: {
+      name: "Suman Bhardwaj",
+      color: "purple",
+    },
+  }),
+]
+});
+</script>
+
+<template>
+  <EditorContent :editor="editor" />
+  
+</template>
